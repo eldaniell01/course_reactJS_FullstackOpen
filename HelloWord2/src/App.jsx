0075d70d36f3211adb04575948b75679b89492d3
eldaniell01 <amontepequem@miumg.edu.gt>
2025-01-08 23:51:17 -0600

    hola mund con react

---
+++ HelloWord2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Hello } from './Hello'
+
+const App = ()=> {
+  
+  return (
+    <>
+      <h1>greettings</h1>
+      <Hello name='daniel'/>
+      <Hello name='daniel'/>
+      <Hello name='daniel'/>
+    </>
+  )
+}
+
+export default App
---
+++ HelloWord2/src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export const Hello = (props)=>{
+    return(
+        <>
+            <p>hola {props.name}</p>
+        </>
+    )
+}
+
+
+
