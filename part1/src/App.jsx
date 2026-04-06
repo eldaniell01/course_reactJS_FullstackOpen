d6113250ef4f9b4d9d60e0f01c8eace47cb3b180
eldaniell01 <amontepequem@miumg.edu.gt>
2026-04-06 05:30:55 -0600

    event state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const App = () => {
+  const [counter, setCouter] = useState(0)
+  setTimeout(
+    ()=> setCouter(counter+1),
+    1000
+  )
+  console.log('rendering... ', counter)
+  return(
+    <div>{counter} </div>
+  )
+}
 
+
+/*
 const Header = (props) =>{
+  console.log(props)
   return(
     <header>
       {props.course}
@@ -18,38 +33,77 @@ const Part = (props) =>{
   )
 }
 
-const Content =(props, part1, part2, part3, exercises1, exercises2, exercises3) =>{
+const Content =(props, parts) =>{
+  console.log(parts)
   return(
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.parts[0]['name']} exercises={props.parts[0]['exercises']}/>
+      <Part part={props.parts[1]['name']} exercises={props.parts[1]['exercises']}/>
+      <Part part={props.parts[2]['name']} exercises={props.parts[2]['exercises']}/>
     </div>
   )
 }
 
-const Total = (props) => {
+const Total = (props, parts) => {
+  var t = 0
+  for (let index = 0; index < props.parts.length; index++) {
+    const element = props.parts[index]['exercises'];
+    t += element
+  }
+  console.log(t)
   return(
     <div>
-      <p> numero de ejercicios {props.ex1+props.ex2+props.ex3}</p>
+      <p> numero de ejercicios {t}</p>
     </div>
   )
 }
 
-const App = ()=> {
-  const course = 'half satack application development'
-  const part1 = 'fundamentals of react'
-  const exercises1 = 10
-  const part2 = 'usign props to pass data'
-  const exercises2 = 7
-  const part3 = 'state of a component'
-  const exercises3 = 14
+const Hello = ({name, age})=>{
+  const bornYear = ()=> new Date().getFullYear()-age
   return(
     <div>
-      <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+      <p>
+        hello {name}, you are {age} years old
+      </p>
+      <p>so you were probably born in {bornYear()} </p>
     </div>
   )
 }
+*/
+
+//const App = (props)=> {
+  /*const course = {
+    name: 'half satack application development',
+    parts: [
+      {
+        name: 'fundamentals of react',
+        exercises: 10
+      },
+      {
+        name: 'usign props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'state of a component',
+        exercises: 14
+      }
+    ]
+  }
+  */ 
+ //const name = 'perez'
+ //const age = 10
+ 
+ //const  {counter} = props
+ // return(
+     {/*<div>
+      {/* <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}/> 
+      {/*<h1>greetings</h1>
+      <Hello name='maya' age={26 +10}/>
+      <Hello name={name} age={age}/>
+      {counter}
+    </div>*/}
+//  )
+//}
 export default App
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot} from 'react-dom/client'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+ReactDom.createRoot(document.getElementById('root')).render(<App/>)
+
+/*let counter = 1
+const root = ReactDom.createRoot(document.getElementById('root'))
+const refresh=()=>{
+  root.render(
+    <App counter= {counter}/>
+  )
+}
+/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App counter = {counter} />
   </StrictMode>,
-)
+)*/
+/*
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)*/
