1950e2e0b9ec6058219dc49aa1bf5c6ee9873f12
eldaniell01 <amontepequem@miumg.edu.gt>
2026-07-01 22:27:28 -0600

    agregando servidor a phonebook

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,17 +6,22 @@ import './App.css'
 import { Numeros } from './components/Numeros'
 import { PersonForm } from './components/FormPerson'
 import { Filter } from './components/Filter'
+import axios from 'axios'
+
 const App=()=> {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName]= useState('')
   const [newPhone, setNewPhone] = useState('')
   const [search, setSearch] = useState('')
 
+  const hook = ()=>{
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response =>{
+          setPersons(response.data)
+        })
+  }
+  useEffect(hook, [])
   const handleChangeInput = (event) => {
     setNewName(event.target.value)
   }
